11670a504eb9cb28fd8fd619079723a1d24cd320
Michael <m.shargorodsky@outlook.com>
2023-01-23 18:58:56 +0000

    (#9): Add patientBar class

--- musicRR.py
+++ musicRR.py
@@ -2,11 +2,19 @@
 
 import numpy as np
 
-def calculate_ratio(data):                     
+class patientBar:
+    def __init__(self, notes, tempo, start, end): 
+        self.notes = notes
+        self.tempo = tempo
+        self.start = start
+        self.end = end
+
+def calculate_ratio(data, start_time):                     
     ratio = []
-    for i in range(len(data)-1):                
+    ratio.append(patientBar(round(data[1]/data[0],4), round(60000/data[0]), start_time, start_time+(data[0]+data[1])/1000))
+    for i in range(1,len(data)-1):                
         value = round(data[i+1]/data[i],4)
-        ratio.append(value)
+        ratio.append(patientBar(value, round(60000/data[i]), ratio[i-1].end, ratio[i-1].end+(data[i]+data[i+1])/1000))
         if i>1500:
             print("Limit reached: 1500 bars")
             break
@@ -14,7 +22,7 @@ def calculate_ratio(data):
 
 def main(patient_file, start_time):
     patient = np.loadtxt(patient_file)
-    rr_ratio = calculate_ratio(patient)     #Calculates RR peak ratios: (N+1)/N
+    rr_ratio = calculate_ratio(patient,start_time)   
     return rr_ratio
 
 if __name__ == "__main__":
